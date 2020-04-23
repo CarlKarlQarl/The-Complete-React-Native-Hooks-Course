764414e051fe0c9c7efe07fae1e051387d597a18
CarlKarlQarl <karlmatthes@gmail.com>
2020-04-23 15:18:30 -0600

    Complete through section 10-103

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,17 +1,40 @@
 import React, { useState } from "react"
 import { View, Text, StyleSheet } from "react-native"
 import SearchBar from "../components/SearchBar"
+import useResults from "../hooks/useResults"
+import ResultsList from "../components/ResultsList"
 
 const SearchScreen = () => {
     const [ term , setTerm ] = useState("")
+    const [ searchAPI, results, errorMessage ] = useResults()
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
+        })
+    }
+
     return (
         <View>
             <SearchBar 
                 term={term} 
                 onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => {}}
+                onTermSubmit={() => searchAPI(term)}
             />
-            <Text>Search Screen</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results.</Text>
+            <ResultsList 
+                title="Cost Effective"
+                results={filterResultsByPrice("$")}
+            />
+            <ResultsList 
+                title="Bit Pricier"
+                results={filterResultsByPrice("$$")}
+            />
+            <ResultsList 
+                title="Big Spender"
+                results={filterResultsByPrice("$$$")}
+            />
         </View>
     )
 }
