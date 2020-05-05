2e28134e64b0f1c65f92d3dfb59bb91e2bf9a925
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-05 12:16:50 -0600

    Complete through section 12-137

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "reac
 import { Context } from "../context/BlogContext"
 import { Feather } from "@expo/vector-icons"
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
     return (
@@ -17,12 +17,14 @@ const IndexScreen = () => {
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                                <Feather name="trash-2" style={styles.icon}/>
-                            </TouchableOpacity>
-                        </View>    
+                        <TouchableOpacity onPress={() => navigation.navigate("Show", {id: item.id})}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather name="trash-2" style={styles.icon}/>
+                                </TouchableOpacity>
+                            </View>    
+                        </TouchableOpacity>
                     )
                 }}
             />
