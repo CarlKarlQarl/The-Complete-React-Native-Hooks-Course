46272c6d835fd659c379fe89bb6cdd2717a7a601
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-13 14:05:06 -0600

    Added POST, DELETE, and PUT routes to API, app is complete, complete through section 13-163

--- blog/src/api/jsonServer.js
+++ blog/src/api/jsonServer.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //Change URL here
-const server = "http://08496cc5.ngrok.io"
+const server = "http://565d1b13.ngrok.io"
 
 export default axios.create({
     baseURL: server
