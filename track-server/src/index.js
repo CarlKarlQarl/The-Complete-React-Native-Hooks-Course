2657fb6cd4f61ecada3846a6655a2c4ab312627f
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-14 18:01:33 -0600

    Set-up connection to mongoDB, complete through section 16-169

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,20 @@
 const express = require("express")
+const mongoose =  require("mongoose")
 
 const app = express()
+
+const mongoURI = `mongodb+srv://Karl123:PasswordPassword123@cluster0-9zazp.mongodb.net/test?retryWrites=true&w=majority`
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo instance")
+})
+mongoose.connection.on("error", (error) => {
+    console.log("Error connecting to mongo", error)
+})
 
 app.get("/", (request, response, next) => {
     response.send("Hi there!")
