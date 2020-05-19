22dd0368b4e0374e80bb444ad4843f02543a9309
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-19 12:01:56 -0600

    Implemented bcrypt for hashing and comparing passwords, sign-in route complete, users able to create new accounts and log-in, complete through section 14-182

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -19,4 +19,25 @@ router.post("/signup", async (request, response, next) => {
     }
 })
 
+router.post("/signin", async (request, response, next) => {
+    const { email, password } = request.body
+
+    if(!email || !password){
+        return response.status(422).send({error: "Must provide email and password"})
+    }
+
+    const user = await User.findOne({email: email})
+    if(!user){
+        return response.status(422).send({error: "Email not found"})
+    }
+
+    try{
+        await user.comparePassword(password)
+        const token = jwt.sign({userID: user._id}, "MY_SECRET_KEY")
+        response.send({token})
+    }catch(error){
+        return response.status(422).send({error: "Invalid password or email"})
+    }
+})
+
 module.exports = router
