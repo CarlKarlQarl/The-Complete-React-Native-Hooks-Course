55b7fd2a628ef03e2696990d8e16526b802a77f5
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-31 17:17:53 -0600

    Added MapView, with initial positioning, complete through section 15-220

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export default axios.create({
     //Change each time you restart ngrok, or every 8 hours
-    baseURL:"http://812f2fb9437f.ngrok.io"
+    baseURL:"http://3b58b40dc0cc.ngrok.io"
 })
