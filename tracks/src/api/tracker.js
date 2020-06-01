684a979c54891c4d88a7dfa335079848d35dace0
CarlKarlQarl <karlmatthes@gmail.com>
2020-05-31 20:00:34 -0600

    Testing adding polylines to map, request permission to track phone location, complete through section 15-224

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export default axios.create({
     //Change each time you restart ngrok, or every 8 hours
-    baseURL:"http://3b58b40dc0cc.ngrok.io"
+    baseURL:"http://df5a26022d1c.ngrok.io"
 })
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,14 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, StyleSheet } from "react-native"
 import { Text } from "react-native-elements"
 import { SafeAreaView } from "react-navigation"
+import { requestPermissionsAsync } from "expo-location"
 import Map from "../components/Map"
 
 const TrackCreateScreen = () => {
+    const [ error, setError ] = useState(null)
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync()
+        } catch(error) {
+            setError(error)
+        }
+    }
+
+    useEffect(() => {
+        startWatching()
+    }, [])
+
     return (
         <SafeAreaView forceInset={{top: "always"}}>
             <Text h2>Create a Track</Text>
             <Map />
+            {error ? <Text>Please enable location services.</Text> : null}
         </SafeAreaView>
     )
 }
