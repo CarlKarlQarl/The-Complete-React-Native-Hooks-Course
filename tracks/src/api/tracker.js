ef22fed20bc8290c93b40f3a40c41984daa0ca0a
CarlKarlQarl <karlmatthes@gmail.com>
2020-06-01 13:07:23 -0600

    Set up mock location data for testing, complete through section 15-227

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export default axios.create({
     //Change each time you restart ngrok, or every 8 hours
-    baseURL:"http://df5a26022d1c.ngrok.io"
+    baseURL:"http://008827ac615e.ngrok.io"
 })
